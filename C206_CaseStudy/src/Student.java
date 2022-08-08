d95c412353dadc30c3ca7fdbd502e3c855ba85e4
21008790 <21008790@10.174.123.189>
2022-08-08 16:38:09 +0800

    COMMIT BY TRICIA WONG

--- C206_CaseStudy/src/Student.java
+++ C206_CaseStudy/src/Student.java
@@ -1,3 +1,4 @@
+//TRICIA WONG
 /**
  * 
  */
--- C206_CaseStudy/src/TuitionManagment.java
+++ C206_CaseStudy/src/TuitionManagment.java
@@ -1,3 +1,4 @@
+//TRICIA WONG
 /**
  * 
  */
@@ -7,6 +8,7 @@
  *
  */
 import java.util.ArrayList;
+
 public class TuitionManagment {
 
 	/**
@@ -14,17 +16,16 @@ public class TuitionManagment {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		ArrayList<Student> studentList = new ArrayList<Student>();
 
 		studentList.add(new Student("Tricia", "female", "92235939", "triciawong19@gmail.com", "03/11/2002", "singapore",
 				"shopping"));
 		studentList.add(new Student("Aricia", "female", "92202937", "ariciawong04@gmail.com", "11/01/2002", "singapore",
 				"running"));
-		studentList.add(new Student("Tommy", "male", "97332937", "tommy@gmail.com", "04/01/2002", "singapore",
-				"running"));
-		
-		
+		studentList
+				.add(new Student("Tommy", "male", "97332937", "tommy@gmail.com", "04/01/2002", "singapore", "running"));
+
 		int option = 0;
 
 		while (option != 4) {
@@ -35,21 +36,19 @@ public class TuitionManagment {
 			if (option == 1) {
 				// View all items
 				TuitionManagment.viewAllStudent(studentList);
-			
 
 			} else if (option == 2) {
 				// Add student
-				TuitionManagment.setHeader("ADD");			
+				TuitionManagment.setHeader("ADD");
 				TuitionManagment.setHeader("DETAILS");
 				System.out.println("1. Student");
 
-				
 				int details = Helper.readInt("Enter option to select item type > ");
 
 				if (details == 1) {
 					// Add student
 					Student stu = inputdetails();
-					TuitionManagment.addStudent(studentList, stu );
+					TuitionManagment.addStudent(studentList, stu);
 					System.out.println("Student added");
 
 				} else {
@@ -58,21 +57,21 @@ public class TuitionManagment {
 
 			} else if (option == 3) {
 				// delete student
-				TuitionManagment.setHeader("DELETE");			
+				TuitionManagment.setHeader("DELETE");
 				TuitionManagment.setHeader("DELETE STUDENTS");
 				System.out.println("1. student");
-		
+
 				int itemType = Helper.readInt("Enter option to select item type > ");
 
 				if (itemType == 1) {
 					// delete students
 					TuitionManagment.deletestudent(studentList);
-		
+					System.out.println("Student deleted");
+
 				} else {
 					System.out.println("Invalid type");
 				}
 
-			
 			} else if (option == 4) {
 				System.out.println("Bye!");
 			} else {
@@ -92,7 +91,7 @@ public class TuitionManagment {
 		Helper.line(80, "-");
 
 	}
-	
+
 	public static void setHeader(String header) {
 		Helper.line(130, "-");
 		System.out.println(header);
@@ -106,24 +105,22 @@ public class TuitionManagment {
 		for (int i = 0; i < studentList.size(); i++) {
 
 			output += String.format("%-10s %-10s %-10s %-30s %-20s %-20s %-20s\n", studentList.get(i).getName(),
-					studentList.get(i).getGender(),
-					studentList.get(i).getMobile(),
-					studentList.get(i).getEmail(),
-					studentList.get(i).getDateofBirth(),
-					studentList.get(i).getCountry(),
+					studentList.get(i).getGender(), studentList.get(i).getMobile(), studentList.get(i).getEmail(),
+					studentList.get(i).getDateofBirth(), studentList.get(i).getCountry(),
 					studentList.get(i).getInterest());
 		}
 		return output;
 	}
+
 	public static void viewAllStudent(ArrayList<Student> studentList) {
 		TuitionManagment.setHeader("STUDENT LIST");
-		String output = String.format("%-10s %-10s %-10s %-30s %-20s %-20s %-20s\n", "NAME", "GENDER",
-				"MOBILE", "EMAIL","DATE OF BIRTH", "COUNTRY", "INTEREST");
-		 output += retrieveAllstudent(studentList);	
+		String output = String.format("%-10s %-10s %-10s %-30s %-20s %-20s %-20s\n", "NAME", "GENDER", "MOBILE",
+				"EMAIL", "DATE OF BIRTH", "COUNTRY", "INTEREST");
+		output += retrieveAllstudent(studentList);
 		System.out.println(output);
 	}
 
-	//================================= Option 2 Add an item (CRUD - Create) =================================
+	// ================================= Option 2 Add an student =================================
 	public static Student inputdetails() {
 		String name = Helper.readString("Enter your name > ");
 		String gender = Helper.readString("Enter your gender > ");
@@ -132,29 +129,29 @@ public class TuitionManagment {
 		String dateofBirth = Helper.readString("Enter your birthday > ");
 		String country = Helper.readString("Enter your country residence > ");
 		String interest = Helper.readString("Enter your interest > ");
-		
-		 Student stu= new Student(name, gender, mobile, email, dateofBirth, country, interest);
+
+		Student stu = new Student(name, gender, mobile, email, dateofBirth, country, interest);
 		return stu;
-		
+
 	}
+
 	public static void addStudent(ArrayList<Student> studentList, Student stu) {
-		
+
 		studentList.add(stu);
 	}
-	
-	//================================= Option 3 delete students (CRUD - Update) =================================
-		public static boolean deletestudent(ArrayList<Student> studentList) {
-			
-			boolean isDeleted = false;
-			String email = Helper.readString("Enter your email > ");
-			for (int i = 0; i < studentList.size(); i++) {
-				if (studentList.get(i).getEmail().contentEquals(email)) {
-					studentList.remove(i);
-				}
-					
-					isDeleted = true;
-					
-				}
-			return isDeleted;
+
+	// ================================= Option 3 delete students =================================
+	public static boolean deletestudent(ArrayList<Student> studentList) {
+
+		boolean isDeleted = false;
+		String email = Helper.readString("Enter your email > ");
+		for (int i = 0; i < studentList.size(); i++) {
+			if (studentList.get(i).getEmail().contentEquals(email)) {
+				studentList.remove(i);
+				isDeleted = true;
+			}
+
 		}
+		return isDeleted;
+	}
 }
