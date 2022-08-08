60b883928a00e9bfc604ede2cbbb286654d274c1
21021888 <21021888@LAPTOP-96PN58IG>
2022-08-08 16:58:57 +0800

    Merge branch 'master' of https://github.com/21025240-Pan-Jia-Cheng/C206_CaseStudy-.git

--- C206_CaseStudy/src/TuitionManagement.java
+++ C206_CaseStudy/src/TuitionManagement.java
@@ -11,11 +11,32 @@ public class TuitionManagement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Tuition> registrationList = new ArrayList<Tuition>();
-		
+		//ArrayList<Tuition> registrationList = new ArrayList<Tuition>();
+		ArrayList<timetable> timetableList = new ArrayList<timetable>();
 		int option = 0;
-		
-		while (option != OPTION_QUIT) {
+		 
+		while (option != 4) { 
+
+			TuitionManagement.menu();
+			option = Helper.readInt("Enter an option > ");
+
+			if (option == 1) {
+				TuitionManagement.viewAlltimetable(timetableList);
+			} else if (option == 2) {
+				timetable tt = inputtimetable();
+				TuitionManagement.addtimetable(timetableList, tt);
+			} else if (option == 3) {
+				String dt = Helper.readString("Enter the tuition timetable id that you want to delete > ");
+				TuitionManagement.deletetimetable(timetableList,dt);
+			} else if (option == 4) {
+				System.out.println("Bye!");
+			} else {
+				System.out.println("Invalid option");
+			}
+
+		}
+		}
+/*		while (option != OPTION_QUIT) {
 			TuitionManagement.menu();
 			option = Helper.readInt("Enter an option > ");
 			
@@ -114,6 +135,81 @@ public class TuitionManagement {
 			System.out.println("Invalid Registration No.");
 		}
 		
+	}*/
+	
+	public static void menu() {
+		TuitionManagement.setHeader("TUITION APP");
+		System.out.println("1. View tuition timetable");
+		System.out.println("2. Add tuition timetable");
+		System.out.println("3. Delete tuition timetable");
+		System.out.println("4. Quit");
+		Helper.line(80, "-");
+
+	}
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
+public static String retrieveAlltimetable(ArrayList<timetable> timetableList) {
+		String output = "";
+
+		for (int i = 0; i < timetableList.size(); i++) {
+
+			output += String.format("%-84s\n", timetableList.get(i).toString()); 
+		}
+		return output;
+	}
+	
+public static void viewAlltimetable(ArrayList<timetable> timetableList) {
+	TuitionManagement.setHeader("TIMETABLE LIST");
+	String output = String.format("%-10s %-10s %-20s %-20s %-20s\n", "ID", "PRICE",
+			"START DATE", "END DATE","MODE");
+	output += 	retrieveAlltimetable(timetableList);
+	System.out.println(output);
 	}
 
+public static timetable inputtimetable() {
+	timetable tt = null;
+	String id = Helper.readString("Enter tuition timetable id > ");
+	double price = Helper.readDouble("Enter the price > ");
+	String startdate = Helper.readString("Enter the start date > ");
+	String enddate = Helper.readString("Enter the end date > ");
+	String mode = Helper.readString("Enter the mode type > ");
+	tt = new timetable(id, price, startdate,enddate, mode);
+	return tt;
+	
+}	
+public static boolean addtimetable(ArrayList<timetable> timetableList, timetable tt) {
+	boolean unique = true;
+	for (timetable i : timetableList) {
+		if (i.getId().contentEquals(tt.getId())) {
+			System.out.println("Error! This tuition ID is duplicated");
+			unique = false;
+		}
+	}
+	if (unique == true){
+		timetableList.add(tt);
+		System.out.println("This tuition ID is now added");
+	}
+	return unique;
 }
+
+public static boolean deletetimetable(ArrayList<timetable> timetableList, String dt) {
+	boolean deleted = false;
+	for (int i = 0; i < timetableList.size(); i++) {
+		String var = timetableList.get(i).getId();
+		if (var.contentEquals(dt)){
+			timetableList.remove(i);
+			System.out.println("Tuition timetable with ID " + var + " was deleted");
+			deleted = true;
+			}
+	}
+	if (deleted == false) {
+		System.out.println("Invalid timetable ID was input.");
+		}
+	return deleted;
+	}
+}
+
+
